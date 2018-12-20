c6d53f8c8a76df3900238e920ba7bb40cac0a5bb
nakadany <daniel_r_b@hotmail.com>
2018-12-20 12:03:39 +0100

    dominio dolor

---
+++ grails-app/domain/salud/Dolor.groovy
@@ -0,0 +1,25 @@
+package salud
+
+class Dolor {
+
+    String nivelDolor
+
+    String lugarDolor
+
+    Tema tema
+
+
+    static constraints = {
+
+        nivelDolor inList: ['Leve', 'Moderado', 'Fuerte'], nullable: false
+        tema nullable: true
+        lugarDolor inList: ['Superior: cabeza', 'Medio: pecho, abdomen', 'Inferior: piernas, pies']
+
+
+
+
+    }
+
+
+
+}
