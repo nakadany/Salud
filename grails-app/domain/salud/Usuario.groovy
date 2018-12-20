3881ddd6eba821fa986e3ca4366248fff60ba48e
nakadany <daniel_r_b@hotmail.com>
2018-12-20 11:43:33 +0100

    dominio usuario

---
+++ grails-app/domain/salud/Usuario.groovy
@@ -0,0 +1,35 @@
+package salud
+
+class Usuario {
+
+
+    String email
+
+    String comprobacionEmail
+
+    String usuario
+
+    String contraseña
+
+    Date ultimoAcceso
+
+    int cantidadIntentosAcceso
+
+    static hasMany = [tema: Tema]
+
+
+    static constraints = {
+
+
+        tema nullable: true
+        cantidadIntentosAcceso nullable: true
+        ultimoAcceso nullable: true
+        usuario size: 5..20, blank: false, nullable: false
+        contraseña size: 5..15, blank: false, nullable: false
+        email email: true, blank: false, unique: true, nullable: false
+
+
+    }
+
+
+}
